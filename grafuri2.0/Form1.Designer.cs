04f546b7e627e7e3d11b1de150716b91722462b1
irina <alexairinamaria@gmail.com>
2025-02-05 21:00:34 +0200

    Add project files.

---
+++ grafuri2.0/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace grafuri2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dfs_btn = new System.Windows.Forms.Button();
+            this.bfs_btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.start_textbox = new System.Windows.Forms.TextBox();
+            this.start_lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1010, 523);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // dfs_btn
+            // 
+            this.dfs_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dfs_btn.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dfs_btn.ForeColor = System.Drawing.Color.HotPink;
+            this.dfs_btn.Location = new System.Drawing.Point(860, 13);
+            this.dfs_btn.Name = "dfs_btn";
+            this.dfs_btn.Size = new System.Drawing.Size(114, 65);
+            this.dfs_btn.TabIndex = 1;
+            this.dfs_btn.Text = "DFS";
+            this.dfs_btn.UseVisualStyleBackColor = true;
+            this.dfs_btn.Click += new System.EventHandler(this.dfs_btn_ClickAsync);
+            // 
+            // bfs_btn
+            // 
+            this.bfs_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bfs_btn.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bfs_btn.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.bfs_btn.Location = new System.Drawing.Point(708, 13);
+            this.bfs_btn.Name = "bfs_btn";
+            this.bfs_btn.Size = new System.Drawing.Size(114, 66);
+            this.bfs_btn.TabIndex = 2;
+            this.bfs_btn.Text = "BFS";
+            this.bfs_btn.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panel1.Controls.Add(this.start_textbox);
+            this.panel1.Controls.Add(this.start_lbl);
+            this.panel1.Controls.Add(this.bfs_btn);
+            this.panel1.Controls.Add(this.dfs_btn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1010, 98);
+            this.panel1.TabIndex = 3;
+            // 
+            // start_textbox
+            // 
+            this.start_textbox.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_textbox.Location = new System.Drawing.Point(177, 35);
+            this.start_textbox.Name = "start_textbox";
+            this.start_textbox.Size = new System.Drawing.Size(100, 42);
+            this.start_textbox.TabIndex = 4;
+            // 
+            // start_lbl
+            // 
+            this.start_lbl.AutoSize = true;
+            this.start_lbl.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_lbl.Location = new System.Drawing.Point(13, 38);
+            this.start_lbl.Name = "start_lbl";
+            this.start_lbl.Size = new System.Drawing.Size(158, 35);
+            this.start_lbl.TabIndex = 3;
+            this.start_lbl.Text = "Nod de start:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 523);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button dfs_btn;
+        private System.Windows.Forms.Button bfs_btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox start_textbox;
+        private System.Windows.Forms.Label start_lbl;
+    }
+}
+
---
+++ grafuri2.0/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace grafuri2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int raza = 20;
+        List<Point> noduri = new List<Point>();
+        bool[,] muchii = new bool[100, 100];
+        bool[,] muchiiParcurse = new bool[100, 100];
+        bool[] viz = new bool[100];
+        int selectedNode = -1;
+
+        private async Task dfs(int node)
+        {
+            viz[node] = true;
+            pictureBox1.Invalidate(); // Redesenare interfață
+            await Task.Delay(500); // Pauză pentru vizualizare
+
+            for (int i = 0; i < noduri.Count; i++) // Nodurile sunt indexate de la 0
+            {
+                if (muchii[node, i] && !viz[i])
+                {
+                    // Colorează muchia dintre `node` și `i`
+                    muchiiParcurse[node,i]=true;
+                    pictureBox1.Invalidate();
+                    await Task.Delay(500);
+
+                    await dfs(i);
+                }
+            }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X; 
+            int y=e.Y;
+            
+            for(int i=0; i<noduri.Count; i++)
+            {
+                if ((Math.Sqrt(Math.Pow((x - noduri[i].X),2) + Math.Pow((y - noduri[i].Y), 2)))<=3*raza)
+                {
+                    if (selectedNode == -1) { 
+                        selectedNode = i;
+
+                        pictureBox1.Invalidate();
+
+                        return;
+                    }
+
+                    if (selectedNode != i)
+                    {
+                        muchii[selectedNode, i] = !muchii[selectedNode, i];
+                        muchii[i, selectedNode] = !muchii[i, selectedNode];
+                    }
+
+                    selectedNode = -1;
+
+                    pictureBox1.Invalidate();
+
+                    return;
+                }
+            }
+
+            noduri.Add(new Point(x,y));
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < noduri.Count; i++)
+            {
+                for (int j = 0; j < noduri.Count; j++)
+                {
+                    if (muchii[i, j])
+                    {
+                        Pen culoare = Pens.Black;
+                        if (muchiiParcurse[i, j] || muchiiParcurse[j,i])
+                        {
+                            culoare = new Pen(Color.Blue, 3); 
+                        }
+                        g.DrawLine(culoare, noduri[i].X, noduri[i].Y, noduri[j].X, noduri[j].Y);
+                    }
+                }
+            }
+
+            for (int i = 0; i < noduri.Count; i++)
+            {
+                Pen culoare = Pens.Black;
+
+                if (viz[i])
+                {
+                    culoare = Pens.Green; 
+                }
+                if (i == selectedNode)
+                {
+                    culoare= Pens.Red; 
+                }
+                g.DrawEllipse(culoare, noduri[i].X - raza, noduri[i].Y - raza, raza * 2, raza * 2);
+
+                string numText = (i+1).ToString();
+                Font font = new Font("Arial", 16);
+                SizeF textSize = g.MeasureString(numText, font);
+                g.DrawString(numText, font, i == selectedNode ? Brushes.Red : Brushes.Black, noduri[i].X - textSize.Width / 2, noduri[i].Y - textSize.Height / 2);
+            }
+        }
+
+        private async Task dfs_btn_ClickAsync(object sender, EventArgs e)
+        {
+            viz = new bool[100];
+            muchiiParcurse = new bool[100, 100];
+            pictureBox1.Invalidate();
+            int nodStart = -1;
+            nodStart = Int32.Parse(start_textbox.Text);
+            if (nodStart!=-1 && (nodStart >= 1 && nodStart<=noduri.Count))
+            {
+                await dfs(nodStart - 1);
+            }
+            else
+            {
+                MessageBox.Show("Introduceți un nod de start valid!");
+            }
+        }
+    }
+}
